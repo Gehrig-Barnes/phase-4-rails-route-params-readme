a6f73b60721d2954db477661e924f1ac8e267e10
Gehrig-Barnes <gehrigbarnes@gmail.com>
2022-02-22 10:43:52 -0600

    commit tree e6083ebdcc1f145b776b992fbc37b35c73204e5a

--- app/models/cheese.rb
+++ app/models/cheese.rb
@@ -3,5 +3,12 @@ class Cheese < ApplicationRecord
   def summary
     "#{self.name}: $#{self.price}"
   end
+  def index
+    cheeses = Cheese.all
+    render json: cheeses
+  end
+
+  def show
+  end
 
 end
